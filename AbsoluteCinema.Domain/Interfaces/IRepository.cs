da41e5e4da0c7780af1645320e2e9e1766530d1f
QusDev <qusdeveloper@gmail.com>
2025-01-27 17:30:32 +0200

    description interfaces

---
+++ AbsoluteCinema.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using AbsoluteCinema.Domain.Entities.Interfaces;
+
+namespace AbsoluteCinema.Domain.Interfaces
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        IQueryable<T> Get(IGetStrategy<T> strategy);
+        Task AddAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task UpdateAsync(T entity);
+    }
+}
---
+++ AbsoluteCinema.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using AbsoluteCinema.Domain.Entities.Interfaces;
+
+namespace AbsoluteCinema.Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> Repository<T>() where T : IEntity;
+        Task SaveChangesAsync();
+    }
+}
