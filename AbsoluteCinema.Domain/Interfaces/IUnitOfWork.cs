c443b4a7bbf3049faea27c13c88b05962cd4a6b1
QusDev <qusdeveloper@gmail.com>
2025-01-28 01:05:32 +0200

    implement unit of work pattern

--- AbsoluteCinema.Domain/Interfaces/IUnitOfWork.cs
+++ AbsoluteCinema.Domain/Interfaces/IUnitOfWork.cs
@@ -4,7 +4,7 @@ namespace AbsoluteCinema.Domain.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
-        IRepository<T> Repository<T>() where T : IEntity;
+        IRepository<T> Repository<T>() where T : class, IEntity;
         Task SaveChangesAsync();
     }
 }
