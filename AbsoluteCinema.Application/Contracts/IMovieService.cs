0d54e48cf423d6d25e5a59293d2dc3a8068f3bcb
QusDev <qusdeveloper@gmail.com>
2025-02-01 19:54:34 +0200

    add basic contract for movie

---
+++ AbsoluteCinema.Application/Contracts/IMovieService.cs
@@ -0,0 +1,13 @@
+﻿using AbsoluteCinema.Application.DTO.Entities;
+
+namespace AbsoluteCinema.Application.Contracts
+{
+    public interface IMovieService
+    {
+        Task<IEnumerable<MovieDto>> GetAllMovies();
+        Task<MovieDto?> GetMovieById(int id);
+        Task DeleteMovie(int id);
+        Task UpdateMovie(MovieDto movie);
+        Task<int> CreateMovie(MovieDto movie);
+    }
+}
