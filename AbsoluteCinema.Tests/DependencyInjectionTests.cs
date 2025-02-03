f0173af6b0ab980e6f5cbe9a941a29fc363ad554
alexander.velikanov <velikanov.exe@gmail.com>
2025-02-03 17:57:28 +0200

    Separate tests logic to layers, delete unnecessary comms

--- AbsoluteCinema.Tests/DependencyInjectionTests.cs
+++ AbsoluteCinema.Tests/DependencyInjectionTests.cs
@@ -1,59 +1,31 @@
 ﻿using AbsoluteCinema.Application;
-using AbsoluteCinema.Application.DTO.AuthDTO;
-using AbsoluteCinema.Application.Validators.AuthValidators;
 using AbsoluteCinema.Domain;
 using AbsoluteCinema.Infrastructure;
-using AutoMapper;
-using FluentValidation;
-using FluentAssertions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Xunit;
 
 namespace AbsoluteCinema.Tests
 {
     public class DependencyInjectionTests
     {
-        private readonly ServiceProvider _serviceProvider;
+        protected readonly ServiceProvider ServiceProvider;
 
-        public DependencyInjectionTests()
+        protected DependencyInjectionTests()
         {
             var services = new ServiceCollection();
 
             // Загружаем тестовую конфигурацию из appsettings.json
-            IConfiguration configuration = GetTestConfiguration();
+            var configuration = GetTestConfiguration();
             
             // Подключаем зависимости 
             services.AddDomainDI();
             services.AddApplicationDI();
             services.AddInfrastructureDI(configuration);
 
-            _serviceProvider = services.BuildServiceProvider();
+            ServiceProvider = services.BuildServiceProvider();
         }
 
-        [Fact]
-        public void Validators_ShouldBeRegistered()
-        {
-            // Получаем валидатор для RegisterDto
-            var validator = _serviceProvider.GetService<IValidator<RegisterDto>>();
-
-            // Проверяем, что валидатор зарегистрирован и имеет корректный тип
-            validator.Should().NotBeNull();
-            validator.Should().BeOfType<RegisterDtoValidator>();
-        }
-
-        [Fact]
-        public void AutoMapper_ShouldHaveValidConfiguration()
-        {
-            // Получаем IMapper
-            var mapper = _serviceProvider.GetService<IMapper>();
-
-            // Проверяем, что AutoMapper зарегистрирован и конфигурация корректна
-            mapper.Should().NotBeNull();
-            mapper.ConfigurationProvider.AssertConfigurationIsValid();
-        }
-        
-        private IConfiguration GetTestConfiguration()
+        private static IConfiguration GetTestConfiguration()
         {
             var webApiPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "..", "..", "..", "AbsoluteCinema.WebAPI"));
             
