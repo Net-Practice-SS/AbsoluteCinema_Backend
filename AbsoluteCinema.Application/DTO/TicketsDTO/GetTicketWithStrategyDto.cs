e17771d7b8225fc8d1d58014cb4c70abbc2352e0
alexander.velikanov <velikanov.exe@gmail.com>
2025-02-12 19:23:39 +0200

    Fix nullable types

--- AbsoluteCinema.Application/DTO/TicketsDTO/GetTicketWithStrategyDto.cs
+++ AbsoluteCinema.Application/DTO/TicketsDTO/GetTicketWithStrategyDto.cs
@@ -2,13 +2,13 @@
 {
     public class GetTicketWithStrategyDto
     {
-        public int Id { get; set; }
-        public int SessionId { get; set; }
-        public int UserId { get; set; }
-        public int Row { get; set; }
-        public int Place { get; set; }
-        public int StatusId { get; set; }
-        public double Price { get; set; }
+        public int? Id { get; set; }
+        public int? SessionId { get; set; }
+        public int? UserId { get; set; }
+        public int? Row { get; set; }
+        public int? Place { get; set; }
+        public int? StatusId { get; set; }
+        public double? Price { get; set; }
 
         public string OrderByProperty { get; set; } = "Id";
         public string OrderDirection { get; set; } = "asc";
